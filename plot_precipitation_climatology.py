01dfacbb79b2fc65c42334e957f260888b9f63d8
Youngil Kim <91korea@gmail.com>
2020-02-09 15:41:34 +0800

    Added realm check and fixed some errors

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -5,7 +5,7 @@ import numpy as np
 import xarray as xr
 import cartopy.crs as ccrs
 import cmocean
-# first modified
+
 
 def convert_pr_units(darray):
     """Convert kg m-2 s-1 to mm day-1.
@@ -15,32 +15,53 @@ def convert_pr_units(darray):
     
     """
     
+    assert darray.units == 'kg m-2 s-1', "Program assumes input units are kg m-2 s-1"
+
     darray.data = darray.data * 86400
     darray.attrs['units'] = 'mm/day'
     
     return darray
 
+def apply_mask(darray, sftlf_file, realm):
+    """Mask ocean or land using a sftlf (land surface fraction) file.
+   
+    Args:
+      darray (xarray.DataArray): Data to mask
+      sftlf_file (str): Land surface fraction file
+      realm (str): Realm to mask
+   
+    """
+  
+    dset = xr.open_dataset(sftlf_file)
+    assert realm in ['land','ocean'],"""Valid realms are 'land' or 'ocean'"""  
+    if realm == 'land':
+        masked_darray = darray.where(dset['sftlf'].data < 50)
+    else:
+        masked_darray = darray.where(dset['sftlf'].data > 50)   
+   
+    return masked_darray
 
-def create_plot(clim, model_name, season, gridlines=False):
+def create_plot(clim, model_name, season, gridlines=False, levels=None):
     """Plot the precipitation climatology.
     
     Args:
       clim (xarray.DataArray): Precipitation climatology data
       model_name (str): Name of the climate model
       season (str): Season
-      
+     
     Kwargs:
-      gridlines (bool): Select whether to plot gridlines    
-      levels (list): Tick marks on the colorbar
+      gridlines (bool): Select whether to plot gridlines
+      levels (list): Tick marks on the colorbar    
     
     """
-        
-	if not levels:
-		levels = np.arange(0,13.5,1.5)
+
+    if not levels:
+        levels = np.arange(0, 13.5, 1.5)
+       
     fig = plt.figure(figsize=[12,5])
     ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
     clim.sel(season=season).plot.contourf(ax=ax,
-                                          levels=np.arange(0, 13.5, 1.5),
+                                          levels=levels,
                                           extend='max',
                                           transform=ccrs.PlateCarree(),
                                           cbar_kwargs={'label': clim.units},
@@ -48,7 +69,7 @@ def create_plot(clim, model_name, season, gridlines=False):
     ax.coastlines()
     if gridlines:
         plt.gca().gridlines()
-
+    
     title = '%s precipitation climatology (%s)' %(model_name, season)
     plt.title(title)
 
@@ -61,23 +82,31 @@ def main(inargs):
     clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
     clim = convert_pr_units(clim)
 
-    create_plot(clim, dset.attrs['model_id'], inargs.season,gridlines=inargs.gridlines, levels=inargs.cbar_levels))
+    if inargs.mask:
+        sftlf_file, realm = inargs.mask
+        clim = apply_mask(clim, sftlf_file, realm)
 
+    create_plot(clim, dset.attrs['model_id'], inargs.season,
+                gridlines=inargs.gridlines, levels=inargs.cbar_levels)
     plt.savefig(inargs.output_file, dpi=200)
 
 
 if __name__ == '__main__':
-    description='Plot the precipitation climatology.'
+    description='Plot the precipitation climatology for a given season.'
     parser = argparse.ArgumentParser(description=description)
-    
+   
     parser.add_argument("pr_file", type=str, help="Precipitation data file")
-    parser.add_argument("season", type=str,choices=['DJF','MAM','JJA','SON'], help="Season to plot")
+    parser.add_argument("season", type=str, help="Season to plot")
     parser.add_argument("output_file", type=str, help="Output file name")
-    parser.add_argument("--cbar_levels",type=float,nargs'*',default=None, help='list of levels / tick marks to appear on the colorbar')
-    parser.add_argument("--gridlines", action="store_true", default=False,help="Include gridlines on the plot")
 
+    parser.add_argument("--gridlines", action="store_true", default=False,
+                        help="Include gridlines on the plot")
+    parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
+                        help='list of levels / tick marks to appear on the colorbar')
+    parser.add_argument("--mask", type=str, nargs=2,
+                        metavar=('SFTLF_FILE', 'REALM'), default=None,
+                        help="""Provide sftlf file and realm to mask ('land' or 'ocean')""")
 
     args = parser.parse_args()
-    
+  
     main(args)
-    
